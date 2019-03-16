fd247a01b90e92af2c9295888ac621157955dc3b
vlad <vturshyn@gmail.com>
2019-03-16 21:02:13 +0200

    Updates

---
+++ precache-manifest.5b777479655fccab018152b8f92754c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "226be7e0e38059d5cfe25253ca80c020",
+    "url": "/proCamp/static/media/logo.226be7e0.png"
+  },
+  {
+    "revision": "3cc6dad60507079251e3",
+    "url": "/proCamp/static/js/runtime~main.a7d5dfaa.js"
+  },
+  {
+    "revision": "c2381e10005547e2421a",
+    "url": "/proCamp/static/js/main.ad54097e.chunk.js"
+  },
+  {
+    "revision": "d14408cb011c7113242e",
+    "url": "/proCamp/static/js/2.4b8ce697.chunk.js"
+  },
+  {
+    "revision": "c2381e10005547e2421a",
+    "url": "/proCamp/static/css/main.0c5562e8.chunk.css"
+  },
+  {
+    "revision": "882f394768587ab9d2d23aa0ce84b48f",
+    "url": "/proCamp/index.html"
+  }
+];
