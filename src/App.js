1a394cef3e8fdc7c52f8f95e112eb9a4562031b9
vlad <vturshyn@gmail.com>
2019-04-01 19:53:50 +0300

    predeploy

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Route, Redirect } from 'react-router'
+
 
 import Header from './components/Header';
 import Movies from './components/Movies';
@@ -17,13 +19,12 @@ class App extends Component {
   render() {
     return (
         <Provider store={store}>
-           <BrowserRouter>
-              <>
+         <BrowserRouter>
                 <Header />
+                <Route exact path="/" render={() => (<Redirect to="/dashboard"/>)}/>
                 <Route path="/dashboard" component={Movies}/>
                 <Route exact path="/film/:id" component={MovieInfo} />
                 <Route path="/film/:id/edit" component={MoviesEdit} />
-              </>
             </BrowserRouter>
         </Provider>
     );
