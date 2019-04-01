f62bbe41ea0ad92f2af75df7d9ceec1f8329ac9d
vlad <vturshyn@gmail.com>
2019-04-01 21:04:18 +0300

    deploy

--- src/App.js
+++ src/App.js
@@ -15,11 +15,11 @@ const store = configureStore();
 class App extends Component {
   render() {
     return (
-      <Provider store={store}>
+      <Provider store={store}>  
         <Router>
           <Header />
           <Route exact path="/" render={() => <Redirect to="/dashboard" />} />
-          <Route exact path="/dashboard" component={Movies} />
+          <Route path="/dashboard" component={Movies} />
           <Route exact path="/film/:id" component={MovieInfo} />
           <Route path="/film/:id/edit" component={MoviesEdit} />
         </Router>
