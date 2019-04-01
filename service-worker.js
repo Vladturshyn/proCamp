61ff1c53305b62bd235dddc549dbc14200de9d7c
vlad <vturshyn@gmail.com>
2019-04-01 19:07:57 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/proCamp/dashboard/precache-manifest.abf45bfb2ea3e80f570deffa36994eec.js"
+  "/proCamp/precache-manifest.338fa66113d21f06edd880a317c5089c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/proCamp/dashboard/index.html", {
+workbox.routing.registerNavigationRoute("/proCamp/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
