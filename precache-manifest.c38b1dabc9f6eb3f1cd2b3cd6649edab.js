0be993714bd6a7675813b5da5afeb3ed25262ac0
vlad <vturshyn@gmail.com>
2019-04-01 19:55:09 +0300

    Updates

---
+++ precache-manifest.c38b1dabc9f6eb3f1cd2b3cd6649edab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "226be7e0e38059d5cfe25253ca80c020",
+    "url": "/static/media/logo.226be7e0.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "66bb05f47f2571067f48",
+    "url": "/static/js/main.5050c77d.chunk.js"
+  },
+  {
+    "revision": "95764a183f14f4f3deba",
+    "url": "/static/js/2.c9fb4a20.chunk.js"
+  },
+  {
+    "revision": "66bb05f47f2571067f48",
+    "url": "/static/css/main.16b396a9.chunk.css"
+  },
+  {
+    "revision": "a5a989ce8efc030e640bd973c55965dc",
+    "url": "/index.html"
+  }
+];
